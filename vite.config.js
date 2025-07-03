e0945a6f884bbdd297012810097e6a1a293f83b3
kentaro456 <118226803+kentaro456@users.noreply.github.com>
2025-07-03 16:23:50 +0200

    Fix: use recommended URL alias for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(new URL(import.meta.url).pathname, '..', 'src'),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
